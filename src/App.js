ad61dc68bcdcf21de57b5d35a4694e3323860b80
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-07-12 18:00:01 -0400

    Called Hobby function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import Hobby from './components/Hobby.js';
 import './App.css';
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="header">
+      <h1>Hobbies</h1>
+    </div>
+    <h2>My favorite hobbie are:</h2>
+      <Hobby name="Running" imageURL="https://i.ibb.co/0MCVvGx/braden-collum-9-HI8-UJMSd-ZA-unsplash.jpg" description="Jogging fast" />
+      <Hobby name="Programming" imageURL="https://i.ibb.co/JpY0PKx/arif-riyanto-v-JP-w-Z6h-GBg-unsplash.jpg" description="Coding software" />
+      <Hobby name="Reading" imageURL="https://i.ibb.co/vHdM2x5/seven-shooter-h-PKTYw-J4-FUo-unsplash.jpg" description="Expressing an interest in books" />
     </div>
   );
 }
